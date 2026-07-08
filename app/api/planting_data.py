8c4aabcdb3dd0e991a44c5367220e7b65d2fb5f7
masgeek <barsamms@gmail.com>
2026-07-08 15:14:18 +0300

    refactor(api): replace `CropData` references with `PlantingRecommendation` in `planting_data.py`
    - Update imports, repository, and response models to align with the renamed data structure.
    - Adjust error logging and data processing to reflect the new terminology.

--- app/api/planting_data.py
+++ app/api/planting_data.py
@@ -2,9 +2,9 @@ from flask import request
 from flask_openapi3 import Tag, APIBlueprint
 
 from app.config import API_PREFIX, API_VERSION
-from app.dto.crop_data_resp import CropDataRecord, PlantingDataResponse, Unauthorized
 from app.dto.data_filters import PlantingDataFilter
-from app.repo.crop_data import CropDataRepo
+from app.dto.planting_recommendation import PlantingRecommendationRecord, PlantingRecommendationResponse, Unauthorized
+from app.repo.planting_recommendation import PlantingRecommendationRepo
 from app.utils.logging import SharedLogger
 
 __bp__ = "/planting-data"
@@ -17,19 +17,19 @@ api = APIBlueprint(__bp__, __name__, url_prefix=url_prefix, abp_tags=[tag])
 shared_logger = SharedLogger()
 logger = shared_logger.get_logger()
 
-planting_data_repo = CropDataRepo()
+repo = PlantingRecommendationRepo()
 
 
 @api.get('',
-         responses={200: PlantingDataResponse, 401: Unauthorized})
+         responses={200: PlantingRecommendationResponse, 401: Unauthorized})
 def get_data(query: PlantingDataFilter):
     page = int(request.args.get('page', default=1, type=int))
     per_page = int(request.args.get('per_page', default=50, type=int))
 
     try:
-        paginated_data = planting_data_repo.get_paginated_data(query, page, per_page)
+        paginated_data = repo.get_paginated_data(query, page, per_page)
 
-        data = [CropDataRecord(
+        data = [PlantingRecommendationRecord(
             id=item.id,
             country=item.country,
             province=item.province,
@@ -52,5 +52,5 @@ def get_data(query: PlantingDataFilter):
         }, 200
 
     except Exception as e:
-        logger.error(f"Error retrieving crop data: {e}")
+        logger.error(f"Error retrieving planting recommendation data: {e}")
         return {'error': str(e)}, 500
