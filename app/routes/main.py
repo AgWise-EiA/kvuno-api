d2853225d5928e4104aafb3e0d631d80f0c5f4a3
masgeek <barsamms@gmail.com>
2026-07-09 07:19:57 +0300

    feat(explore): add leaflet/bootstrap-icons deps, /ui/explore route, navbar link

--- app/routes/main.py
+++ app/routes/main.py
@@ -305,6 +305,10 @@ def register_app_routes(app):
     def ui_jobs_html():
         return render_template('jobs.html', active_nav='jobs')
 
+    @app.route('/ui/explore', methods=['GET'])
+    def ui_explore():
+        return render_template('explore.html', active_nav='explore')
+
     @app.route('/ui/jobs/data', methods=['GET'])
     def ui_jobs_data():
         jobs = _load_jobs()
