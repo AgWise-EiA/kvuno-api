f6705ad6bdaad51feb0a2cefc49fb09fc5e93198
masgeek <barsamms@gmail.com>
2026-07-09 07:45:35 +0300

    feat: add `is_specified_db` utility to check database dialect in migration utils

--- app/utils/migration_utils.py
+++ app/utils/migration_utils.py
@@ -2,6 +2,11 @@ from alembic import op
 from sqlalchemy import Integer, BigInteger
 
 
+def is_specified_db(db_dialect):
+    dialect = op.get_bind().dialect.name
+    return dialect == db_dialect
+
+
 def get_integer_column_type():
     """
     Returns the appropriate SQLAlchemy column type for the primary key based on the database dialect.
